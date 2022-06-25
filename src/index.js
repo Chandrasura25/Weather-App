1b94e6b78d2978cec9f768f2952901f0130ba040
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2022-06-25 23:01:28 +0100

    trying service worker

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
-import "../node_modules/font-awesome/css/font-awesome.css"
+import {BrowserRouter} from 'react-router-dom';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import "../node_modules/font-awesome/css/font-awesome.css";
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -13,6 +14,10 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
